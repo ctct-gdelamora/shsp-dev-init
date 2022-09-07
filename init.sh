5799232b5e6aa2552028a0b8045997f9ebac4a70
Grace de la Mora <grace.delamora@sharpspring.com>
2022-09-07 14:27:58 -0400

    still fixing homebrew install

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ fi
 
 # Install Homebrew
 if ! which brew >/dev/null; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Installing Homebrew..."
 fi
 
