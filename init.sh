8a8529a239a04cdab6475200406fa7386ef3b6b9
Grace de la Mora <grace.delamora@sharpspring.com>
2022-09-08 16:07:52 -0400

    testing app install

--- init.sh
+++ init.sh
@@ -6,54 +6,55 @@
 # unofficial bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-# Install XCode
-if ! which xcode-select >/dev/null; then
-    xcode-select --install
-else
-    echo "Xcode-Select already installed. Skipping."
-fi
+# Commenting all this out since I know it works
+# # Install XCode
+# if ! which xcode-select >/dev/null; then
+#     xcode-select --install
+# else
+#     echo "Xcode-Select already installed. Skipping."
+# fi
 
-# Install Homebrew or re-tap homebrew/core if already installed.
-if ! which brew >/dev/null; then
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
-    source ~/.zshrc
-else
-    echo "Homebrew already installed. Updating..."
-    brew update
-    echo "Re-tapping homebrew/core."
-    rm -rf "$(brew --repo homebrew/core)"
-    brew tap homebrew/core
-fi
+# # Install Homebrew or re-tap homebrew/core if already installed.
+# if ! which brew >/dev/null; then
+#     echo "Installing Homebrew..."
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
+#     source ~/.zshrc
+# else
+#     echo "Homebrew already installed. Updating..."
+#     brew update
+#     echo "Re-tapping homebrew/core."
+#     rm -rf "$(brew --repo homebrew/core)"
+#     brew tap homebrew/core
+# fi
 
-# Install NVM or skip
-if ! which nvm >/dev/null; then
-    echo "Node Version Manager not found. Installing."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    source ~/.zshrc
-else
-    echo "Node Version Manager already installed. Skipping."
-fi
+# # Install NVM or skip
+# if ! which nvm >/dev/null; then
+#     echo "Node Version Manager not found. Installing."
+#     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#     source ~/.zshrc
+# else
+#     echo "Node Version Manager already installed. Skipping."
+# fi
 
-# Regardless of results above, install NVM 14
-echo "Installing Node v14, current version supported."
-nvm install 14
+# # Regardless of results above, install NVM 14
+# echo "Installing Node v14, current version supported."
+# nvm install 14
 
-# Install gettext
-if ! which gettext >/dev/null; then
-    echo "gettext not found. Installing."
-    brew install gettext
-else
-    echo "gettext already installed. Skipping."
-fi
+# # Install gettext
+# if ! which gettext >/dev/null; then
+#     echo "gettext not found. Installing."
+#     brew install gettext
+# else
+#     echo "gettext already installed. Skipping."
+# fi
 
-if ! which zsh >/dev/null; then
-    echo "zsh not found. Installing."
-    brew install zsh
-else
-    echo "zsh already installed. Skipping."
-fi
+# if ! which zsh >/dev/null; then
+#     echo "zsh not found. Installing."
+#     brew install zsh
+# else
+#     echo "zsh already installed. Skipping."
+# fi
 
 if  [ -e "/Applications/Visual Studio Code.app" ]; then 
     echo "Visual Studio Code already installed. Skipping."
