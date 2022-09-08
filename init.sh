7e815c23f46a8efa5aa6f9014645e2da9e3a73a3
Grace de la Mora <grace.delamora@sharpspring.com>
2022-09-08 11:29:33 -0400

    testing the rest of the install script

--- init.sh
+++ init.sh
@@ -16,7 +16,6 @@ fi
 # Install Homebrew
 if ! which brew >/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Installing Homebrew..."
     echo "eval '$(/opt/homebrew/bin/brew shellenv)'" >> /Users/"$(whoami)"/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
@@ -29,55 +28,54 @@ source /Users/"$(whoami)"/.zprofile
 # sanity check, read brew version
 brew -v
 
-# # CLI Tools
-# PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n') || true
+# CLI Tools
+PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n') || true
 
-# if [[ ! -z "$PROD" ]]; then
-#   softwareupdate -i "$PROD" --verbose
-# fi
+if [[ ! -z "$PROD" ]]; then
+  softwareupdate -i "$PROD" --verbose
+fi
 
-# PACKAGES=(
-#     gettext
-#     zsh
-# )
+PACKAGES=(
+    gettext
+    zsh
+)
 
-# echo "Installing packages..."
-# brew install "${PACKAGES[@]}"
+echo "Installing packages..."
+brew install "${PACKAGES[@]}"
 
-# CASKS=(
-#     firefox
-#     google-chrome
-#     iterm2
-#     slack
-#     visual-studio-code
-#     zoom
-# )
+CASKS=(
+    firefox
+    google-chrome
+    iterm2
+    slack
+    visual-studio-code
+    zoom
+)
 
-# echo "Installing cask apps (Firefox, Google Chrome, iTerm2, Slack, Visual Studio Code, Zoom)..."
-# sudo -u $SUDO_USER brew install --cask ${CASKS[@]}
+echo "Installing cask apps (Firefox, Google Chrome, iTerm2, Slack, Visual Studio Code, Zoom)..."
+sudo -u $SUDO_USER brew install --cask ${CASKS[@]}
 
-# cd ~/
-# git clone https://github.com/nvm-sh/nvm.git .nvm
-# . ./nvm.sh
+cd ~/
+git clone https://github.com/nvm-sh/nvm.git .nvm
+. ./nvm.sh
 
-# cat <<EOF >> ~/.zshrc
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# EOF
+cat <<EOF >> ~/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
 
-# source ~/.zshrc
+source ~/.zshrc
 
-# nvm install 14 # CURRENT NODE VERSION 14 SUPPORTED BY SHSP
+nvm install 14 # CURRENT NODE VERSION 14 SUPPORTED BY SHSP
 
-# # Rancher Desktop
-# ## Download
-# curl -O ~/Downloads/RancherDesktop.dmg https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.4.1/Rancher.Desktop-1.4.1.aarch64.dmg
-# ## Mount the .dmg image
-# sudo hdiutil attach ~/Downloads/RancherDesktop.dmg
-# ## Install the application
-# cp -R /Volumes/RancherDesktop/RancherDesktop.app /Applications
-# diskutil unmount /Volumes/RancherDesktop
-# open /Applications/RancherDesktop.app
-
+# Rancher Desktop
+## Download
+curl -O ~/Downloads/RancherDesktop.dmg https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.4.1/Rancher.Desktop-1.4.1.aarch64.dmg
+## Mount the .dmg image
+sudo hdiutil attach ~/Downloads/RancherDesktop.dmg
+## Install the application
+cp -R /Volumes/RancherDesktop/RancherDesktop.app /Applications
+diskutil unmount /Volumes/RancherDesktop
+open /Applications/RancherDesktop.app
 }
