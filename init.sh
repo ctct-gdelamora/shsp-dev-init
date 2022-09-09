23a1a121583eec2655499cbaecc71e37abddfba1
Grace de la Mora <grace.delamora@sharpspring.com>
2022-09-09 13:04:54 -0400

    testing a flag

--- init.sh
+++ init.sh
@@ -57,7 +57,7 @@ set -euo pipefail
 # fi
 
 # Check if VS Code is installed
-if  [ ! -e "/Applications/Visual Studio Code.app" ]; then 
+if  [ ! -f "/Applications/Visual Studio Code.app" ]; then 
     read -p "Visual Studio Code is the recommended IDE. Install? [Y/n]\t" -n 1 -r
     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         brew install --cask visual-studio-code;
