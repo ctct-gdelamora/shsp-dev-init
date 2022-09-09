7f7689706ec5bf93ee48ff21834eb142a12658e4
Grace de la Mora <grace.delamora@sharpspring.com>
2022-09-09 16:32:32 -0400

    testing

--- init.sh
+++ init.sh
@@ -11,7 +11,6 @@ if ! which xcode-select >/dev/null; then
     xcode-select --install
 else
     echo "Xcode-Select already installed. Skipping."
-    exit 0
 fi
 
 # Install Homebrew or re-tap homebrew/core if already installed.
@@ -26,7 +25,6 @@ else
     echo "Re-tapping homebrew/core."
     rm -rf "$(brew --repo homebrew/core)"
     brew tap homebrew/core
-    exit 0
 fi
 
 # Install NVM or skip
@@ -36,7 +34,6 @@ if ! which nvm >/dev/null; then
     source ~/.zshrc
 else
     echo "Node Version Manager already installed. Skipping."
-    exit 0
 fi
 
 # Regardless of results above, install NVM 14
@@ -49,7 +46,6 @@ if ! which gettext >/dev/null; then
     brew install gettext
 else
     echo "gettext already installed. Skipping."
-    exit 0
 fi
 
 # Install zsh if not installed
@@ -58,78 +54,78 @@ if ! which zsh >/dev/null; then
     brew install zsh
 else
     echo "zsh already installed. Skipping."
-    exit 0
 fi
 
-# Check if iTerm2 is installed
-if [ ! -f "/Applications/iTerm 2.app" ]; then
-    read -n 1 -r -p "iTerm2 is an optional install. Install? [Y/n]"
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        echo
-        brew install --cask iterm2
-    else
-        echo
-        echo "iTerm2 will not be installed. Skipping.";
-    fi
-fi
+# none of this works and I don't know why - leaving it in for future investigation
+# # Check if iTerm2 is installed
+# if [ ! -f "/Applications/iTerm 2.app" ]; then
+#     read -n 1 -r -p "iTerm2 is an optional install. Install? [Y/n]"
+#     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+#         echo
+#         brew install --cask iterm2
+#     else
+#         echo
+#         echo "iTerm2 will not be installed. Skipping.";
+#     fi
+# fi
 
-# REQUIRED: Check if Rancher Desktop is installed
-if [ ! -f "/Applications/Rancher Desktop.app" ]; then
-    echo "Rancher Desktop is a required M1 install. Downloading."
-    ## Download
-    curl -O ~/Downloads/RancherDesktop.dmg https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.4.1/Rancher.Desktop-1.4.1.aarch64.dmg
-    ## Mount the .dmg image
-    sudo hdiutil attach ~/Downloads/RancherDesktop.dmg
-    ## Install the application
-    cp -R /Volumes/RancherDesktop/RancherDesktop.app /Applications
-    diskutil unmount /Volumes/RancherDesktop
-    open /Applications/RancherDesktop.app
-else
-    echo "Rancher Desktop already installed. Skipping."
-fi
+# # REQUIRED: Check if Rancher Desktop is installed
+# if [ ! -f "/Applications/Rancher Desktop.app" ]; then
+#     echo "Rancher Desktop is a required M1 install. Downloading."
+#     ## Download
+#     curl -O ~/Downloads/RancherDesktop.dmg https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.4.1/Rancher.Desktop-1.4.1.aarch64.dmg
+#     ## Mount the .dmg image
+#     sudo hdiutil attach ~/Downloads/RancherDesktop.dmg
+#     ## Install the application
+#     cp -R /Volumes/RancherDesktop/RancherDesktop.app /Applications
+#     diskutil unmount /Volumes/RancherDesktop
+#     open /Applications/RancherDesktop.app
+# else
+#     echo "Rancher Desktop already installed. Skipping."
+# fi
 
-# REQUIRED: Check if Slack is installed
-if [ ! -f "/Applications/Slack.app" ]; then
-    echo "Slack is required for intra-company communications. Downloading."
-        brew install --cask slack
-else
-    echo "Slack already installed. Skipping."
-fi
+# # REQUIRED: Check if Slack is installed
+# if [ ! -f "/Applications/Slack.app" ]; then
+#     echo "Slack is required for intra-company communications. Downloading."
+#         brew install --cask slack
+# else
+#     echo "Slack already installed. Skipping."
+# fi
 
-# Check if VS Code is installed
-if [ ! -f "/Applications/Visual Studio Code.app" ]; then
-    read -n 1 -r -p "Visual Studio Code is the recommended IDE. Install? [Y/n]"
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        echo
-        brew install --cask visual-studio-code
-    else
-        echo
-        echo "Visual Studio Code will not be installed. Skipping.";
-    fi
-fi
+# # Check if VS Code is installed
+# if [ ! -f "/Applications/Visual Studio Code.app" ]; then
+#     read -n 1 -r -p "Visual Studio Code is the recommended IDE. Install? [Y/n]"
+#     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+#         echo
+#         brew install --cask visual-studio-code
+#     else
+#         echo
+#         echo "Visual Studio Code will not be installed. Skipping.";
+#     fi
+# fi
 
-# Check if Chrome is installed
-if [ ! -f "/Applications/Google Chrome.app" ]; then
-    read -n 1 -r -p "Google Chrome is recommended for browser testing. Install? [Y/n]"
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        echo
-        brew install --cask google-chrome
-    else
-        echo
-        echo "Google Chrome will not be installed. Skipping.";
-    fi
-fi
+# # Check if Chrome is installed
+# if [ ! -f "/Applications/Google Chrome.app" ]; then
+#     read -n 1 -r -p "Google Chrome is recommended for browser testing. Install? [Y/n]"
+#     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+#         echo
+#         brew install --cask google-chrome
+#     else
+#         echo
+#         echo "Google Chrome will not be installed. Skipping.";
+#     fi
+# fi
 
-# Check if Zoom is installed
-if [ ! -f "/Applications/Zoom.app" ]; then
-    read -n 1 -r -p "Zoom is required for video meetings. Install? [Y/n]"
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        echo
-        brew install --cask zoom
-    else
-        echo
-        echo "Zoom will not be installed. Skipping.";
-    fi
-fi
+# # Check if Zoom is installed
+# if [ ! -f "/Applications/Zoom.app" ]; then
+#     read -n 1 -r -p "Zoom is required for video meetings. Install? [Y/n]"
+#     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+#         echo
+#         brew install --cask zoom
+#     else
+#         echo
+#         echo "Zoom will not be installed. Skipping.";
+#     fi
+# fi
 
 }
